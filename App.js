6a41db31be9228da238b674c7f864bd66e28ac9a
Radouan <rachidi.radouan19@gmail.com>
2023-06-15 12:38:29 +0200

    Maj carte + scan livre et affichage des boites

--- App.js
+++ App.js
@@ -1,91 +1,200 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import { Text, View, StyleSheet, Button, Dimensions } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
+import MapView, { Marker } from 'react-native-maps';
 import data from './data';
+
+const LOGIN_TIMEOUT = 10 * 60 * 1000; // 10 minutes
 
 export default function App() {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
-  const [text, setText] = useState('Not yet scanned');
+  const [text, setText] = useState('Non encore scanné');
   const [selectedBox, setSelectedBox] = useState(null);
   const [selectedBook, setSelectedBook] = useState(null);
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [user, setUser] = useState(null);
+  const [lastLoginTime, setLastLoginTime] = useState(null);
 
-  const askForCameraPermission = () => {
-    (async () => {
-      const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
-    })();
+  const askForCameraPermission = async () => {
+    const { status } = await BarCodeScanner.requestPermissionsAsync();
+    setHasPermission(status === 'granted');
   };
 
-  // Request Camera Permission
   useEffect(() => {
     askForCameraPermission();
   }, []);
 
-  // What happens when we scan the bar code
-  const handleBarCodeScanned = ({ type, data: scannedData }) => {
-    setScanned(true);
-    setText(scannedData);
-  
-    // Find the box based on the scanned data (ID)
-    const selectedBox = data.boxes.find((box) => box.books.includes(parseInt(scannedData)));
-  
-    if (selectedBox) {
-      setSelectedBox(selectedBox);
-  
-      // Find the book based on the box's book ID
-      const selectedBook = data.books.find((book) => book.id === parseInt(scannedData));
-  
+  useEffect(() => {
+    if (loggedIn) {
+      setLastLoginTime(Date.now());
+    }
+  }, [loggedIn]);
+
+  const handleBarCodeScanned = ({ data: scannedData }) => {
+    if (!loggedIn && scannedData === '1234567891012345') {
+      setLoggedIn(true);
+      setUser({ firstName: 'Tom', lastName: 'Gérard' });
+      setScanned(false);
+      setText('Connexion réussie !');
+    } else if (loggedIn && selectedBox && !selectedBook) {
+      const bookId = parseInt(scannedData);
+      const selectedBook = selectedBox.books.find((book) => book.id === bookId);
+
       if (selectedBook) {
         setSelectedBook(selectedBook);
+        selectedBox.books = selectedBox.books.filter((book) => book.id !== bookId);
+        setScanned(false);
+        setText('Livre sélectionné ! Scannez un autre livre ou déposez le livre actuel.');
+      } else {
+        setScanned(false);
+        setText('ID de livre invalide. Veuillez scanner un autre livre.');
       }
     }
-  
-    console.log('Type: ' + type + '\nData: ' + scannedData);
   };
 
-  // Check permissions and return the screens
+  const handleBookTake = () => {
+    if (selectedBox && selectedBook) {
+      setSelectedBook(null);
+      setText('Livre pris avec succès !');
+      // Vérifier le délai de connexion
+      if (lastLoginTime && Date.now() - lastLoginTime > LOGIN_TIMEOUT) {
+        // Si le délai de connexion est dépassé, réinitialiser l'état de connexion
+        setLoggedIn(false);
+        setUser(null);
+        setLastLoginTime(null);
+      } else {
+        // Réinitialiser l'état de la boîte sélectionnée pour revenir à la sélection sur la carte
+        setSelectedBox(null);
+      }
+    }
+  };
+
+  const handleBookDeposit = () => {
+    if (selectedBox && selectedBook) {
+      selectedBox.books.push(selectedBook);
+      setSelectedBook(null);
+      setText('Livre déposé avec succès !');
+    }
+  };
+
   if (hasPermission === null) {
     return (
       <View style={styles.container}>
-        <Text>Requesting camera permission</Text>
+        <Text style={styles.instruction}>Demande de permission pour la caméra...</Text>
       </View>
     );
   }
+
   if (hasPermission === false) {
     return (
       <View style={styles.container}>
-        <Text style={{ margin: 10 }}>No access to camera</Text>
-        <Button title={'Allow Camera'} onPress={() => askForCameraPermission()} />
+        <Text style={styles.instruction}>Pas d'accès à la caméra</Text>
+        <Button title={'Autoriser la caméra'} onPress={() => askForCameraPermission()} />
       </View>
     );
   }
 
-  if (selectedBox && selectedBook) {
+  if (!loggedIn) {
     return (
       <View style={styles.container}>
-        <Text>Box: {selectedBox.location}</Text>
-        <Text>Title: {selectedBook.title}</Text>
-        <Text>Author: {selectedBook.author}</Text>
-        <Button title={'Scan again?'} onPress={() => setScanned(false)} color="tomato" />
+        <Text style={styles.instruction}>Veuillez scanner le code QR pour vous connecter</Text>
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          style={styles.scanner}
+        />
+        {scanned && (
+          <Button
+            title={'Scanner à nouveau ?'}
+            onPress={() => {
+              setScanned(false);
+              setText('Non encore scanné');
+            }}
+            color="tomato"
+          />
+        )}
+        <Text style={styles.text}>{text}</Text>
       </View>
     );
   }
 
-  // Return the default scanner view
-  return (
-    <View style={styles.container}>
-      <View style={styles.barcodebox}>
+  if (loggedIn && !selectedBox) {
+    return (
+      <View style={styles.container}>
+        <MapView style={styles.map} initialRegion={{ latitude: 48.8566, longitude: 2.3522, latitudeDelta: 8, longitudeDelta: 8 }}>
+          {data.boxes.map((box) => (
+            <Marker
+              key={box.id}
+              coordinate={{ latitude: box.latitude, longitude: box.longitude }}
+              onPress={() => setSelectedBox(box)}
+            />
+          ))}
+        </MapView>
+        <Text style={styles.instruction}>Bienvenue, {user.firstName} {user.lastName} !</Text>
+        <Text style={styles.instruction}>Sélectionnez une boîte pour voir ses livres</Text>
+        <Text style={styles.text}>{text}</Text>
+      </View>
+    );
+  }
+
+  if (loggedIn && selectedBox && !selectedBook) {
+    const renderBooks = () => {
+      return (
+        <View>
+          <Text style={styles.instruction}>Liste des livres :</Text>
+          {selectedBox.books.map((book) => (
+            <Text key={book.id}>{book.title}</Text>
+          ))}
+        </View>
+      );
+    };
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.instruction}>Boîte : {selectedBox.location}</Text>
+        {renderBooks()}
+        <Text style={styles.instruction}>Sélectionnez un livre en scannant son code QR</Text>
         <BarCodeScanner
           onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-          style={{ height: 400, width: 400 }}
+          style={styles.scanner}
         />
+        {scanned && (
+          <Button
+            title={'Scanner à nouveau ?'}
+            onPress={() => {
+              setScanned(false);
+              setText('Non encore scanné');
+            }}
+            color="tomato"
+          />
+        )}
+        <Text style={styles.text}>{text}</Text>
       </View>
-      <Text style={styles.maintext}>{text}</Text>
+    );
+  }
 
-      {scanned && <Button title={'Scan again?'} onPress={() => setScanned(false)} color="tomato" />}
-    </View>
-  );
+  if (loggedIn && selectedBox && selectedBook) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.instruction}>Boîte : {selectedBox.location}</Text>
+        <Text style={styles.instruction}>Titre : {selectedBook.title}</Text>
+        <Text style={styles.instruction}>Auteur : {selectedBook.author}</Text>
+        <Button
+          title={'Scanner un autre livre ?'}
+          onPress={() => {
+            setSelectedBook(null);
+            setText('Non encore scanné');
+          }}
+          color="tomato"
+        />
+        <Button title={'Prendre le livre'} onPress={handleBookTake} color="green" />
+        <Button title={'Déposer le livre'} onPress={handleBookDeposit} color="blue" />
+        <Text style={styles.text}>{text}</Text>
+      </View>
+    );
+  }
+
+  return null;
 }
 
 const styles = StyleSheet.create({
@@ -95,17 +204,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  maintext: {
-    fontSize: 16,
-    margin: 20,
+  instruction: {
+    fontSize: 20,
+    marginBottom: 10,
   },
-  barcodebox: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 300,
-    width: 300,
-    overflow: 'hidden',
-    borderRadius: 30,
-    backgroundColor: 'tomato',
+  scanner: {
+    width: Dimensions.get('window').width - 20,
+    height: Dimensions.get('window').height / 2,
+    marginBottom: 10,
+  },
+  map: {
+    flex: 1,
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+  text: {
+    fontSize: 18,
+    marginTop: 10,
   },
 });
--- data.js
+++ data.js
@@ -3,86 +3,119 @@ const data = {
     {
       id: 1,
       location: 'Paris',
-      books: [1, 2, 3]
+      latitude: 48.8566,
+      longitude: 2.3522,
+      books: [
+        {
+          id: 1,
+          title: 'Book 1',
+          author: 'Author 1',
+        },
+        {
+          id: 2,
+          title: 'Book 2',
+          author: 'Author 2',
+        },
+        {
+          id: 3,
+          title: 'Book 3',
+          author: 'Author 3',
+        },
+      ],
     },
     {
       id: 2,
       location: 'Lyon',
-      books: [4, 5]
+      latitude: 45.75,
+      longitude: 4.85,
+      books: [
+        {
+          id: 4,
+          title: 'Book 4',
+          author: 'Author 4',
+        },
+        {
+          id: 5,
+          title: 'Book 5',
+          author: 'Author 5',
+        },
+        {
+          id: 6,
+          title: 'Book 6',
+          author: 'Author 6',
+        },
+      ],
     },
     {
       id: 3,
       location: 'Bordeaux',
-      books: [6, 7]
+      latitude: 44.8378,
+      longitude: -0.5792,
+      books: [
+        {
+          id: 7,
+          title: 'Book 7',
+          author: 'Author 7',
+        },
+        {
+          id: 8,
+          title: 'Book 8',
+          author: 'Author 8',
+        },
+        {
+          id: 9,
+          title: 'Book 9',
+          author: 'Author 9',
+        },
+      ],
     },
     {
       id: 4,
       location: 'Toulouse',
-      books: [8, 9]
+      latitude: 43.6047,
+      longitude: 1.4442,
+      books: [
+        {
+          id: 10,
+          title: 'Book 10',
+          author: 'Author 10',
+        },
+        {
+          id: 11,
+          title: 'Book 11',
+          author: 'Author 11',
+        },
+        {
+          id: 12,
+          title: 'Book 12',
+          author: 'Author 12',
+        },
+      ],
     },
     {
       id: 5,
       location: 'Lille',
-      books: [10, 11]
-    }
+      latitude: 50.6292,
+      longitude: 3.0573,
+      books: [
+        {
+          id: 13,
+          title: 'Book 13',
+          author: 'Author 13',
+        },
+        {
+          id: 14,
+          title: 'Book 14',
+          author: 'Author 14',
+        },
+        {
+          id: 15,
+          title: 'Book 15',
+          author: 'Author 15',
+        },
+      ],
+    },
   ],
-  books: [
-    {
-      id: 1,
-      title: 'Book 1',
-      author: 'Author 1'
-    },
-    {
-      id: 2,
-      title: 'Book 2',
-      author: 'Author 2'
-    },
-    {
-      id: 3,
-      title: 'Book 3',
-      author: 'Author 3'
-    },
-    {
-      id: 4,
-      title: 'Book 4',
-      author: 'Author 4'
-    },
-    {
-      id: 5,
-      title: 'Book 5',
-      author: 'Author 5'
-    },
-    {
-      id: 6,
-      title: 'Book 6',
-      author: 'Author 6'
-    },
-    {
-      id: 7,
-      title: 'Book 7',
-      author: 'Author 7'
-    },
-    {
-      id: 8,
-      title: 'Book 8',
-      author: 'Author 8'
-    },
-    {
-      id: 9,
-      title: 'Book 9',
-      author: 'Author 9'
-    },
-    {
-      id: 10,
-      title: 'Book 10',
-      author: 'Author 10'
-    },
-    {
-      id: 11,
-      title: 'Book 11',
-      author: 'Author 11'
-    }
-  ]
 };
 
 export default data;
