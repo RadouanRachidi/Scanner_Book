6fbb6ae869f0db93509bb824afa2f2ae754b7f42
Radouan <rachidi.radouan19@gmail.com>
2023-09-04 14:00:51 +0200

    ajout titre concret

--- App.js
+++ App.js
@@ -7,219 +7,238 @@ import data from './data';
 const LOGIN_TIMEOUT = 10 * 60 * 1000; // 10 minutes
 
 export default function App() {
-  const [hasPermission, setHasPermission] = useState(null);
-  const [scanned, setScanned] = useState(false);
-  const [text, setText] = useState('Non encore scanné');
-  const [selectedBox, setSelectedBox] = useState(null);
-  const [selectedBook, setSelectedBook] = useState(null);
-  const [loggedIn, setLoggedIn] = useState(false);
-  const [user, setUser] = useState(null);
-  const [lastLoginTime, setLastLoginTime] = useState(null);
+    const [hasPermission, setHasPermission] = useState(null);
+    const [scanned, setScanned] = useState(false);
+    const [text, setText] = useState('Non encore scanné');
+    const [selectedBox, setSelectedBox] = useState(null);
+    const [selectedBook, setSelectedBook] = useState(null);
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [user, setUser] = useState(null);
+    const [lastLoginTime, setLastLoginTime] = useState(null);
 
-  const askForCameraPermission = async () => {
-    const { status } = await BarCodeScanner.requestPermissionsAsync();
-    setHasPermission(status === 'granted');
-  };
-
-  useEffect(() => {
-    askForCameraPermission();
-  }, []);
-
-  useEffect(() => {
-    if (loggedIn) {
-      setLastLoginTime(Date.now());
-    }
-  }, [loggedIn]);
-
-  const handleBarCodeScanned = ({ data: scannedData }) => {
-    if (!loggedIn && scannedData === '1234567891012345') {   
-      setLoggedIn(true);
-      setUser({ firstName: 'Tom', lastName: 'Gérard' });
-      setScanned(false);
-      setText('Connexion réussie !');
-    } else if (loggedIn && selectedBox && !selectedBook) {
-      const bookId = parseInt(scannedData);
-      const selectedBook = selectedBox.books.find((book) => book.id === bookId);
-
-      if (selectedBook) {
-        setSelectedBook(selectedBook);
-        selectedBox.books = selectedBox.books.filter((book) => book.id !== bookId);
-        setScanned(false);
-        setText('Livre sélectionné ! Scannez un autre livre ou déposez le livre actuel.');
-      } else {
-        setScanned(false);
-        setText('ID de livre invalide. Veuillez scanner un autre livre.');
-      }
-    }
-  };
-
-  const handleBookTake = () => {
-    if (selectedBox && selectedBook) {
-      setSelectedBook(null);
-      setText('Livre pris avec succès !');
-      // Vérifier le délai de connexion
-      if (lastLoginTime && Date.now() - lastLoginTime > LOGIN_TIMEOUT) {
-        // Si le délai de connexion est dépassé, réinitialiser l'état de connexion
-        setLoggedIn(false);
-        setUser(null);
-        setLastLoginTime(null);
-      } else {
-        // Réinitialiser l'état de la boîte sélectionnée pour revenir à la sélection sur la carte
-        setSelectedBox(null);
-      }
-    }
-  };
-
-  const handleBookDeposit = () => {
-    if (selectedBox && selectedBook) {
-      selectedBox.books.push(selectedBook);
-      setSelectedBook(null);
-      setText('Livre déposé avec succès !');
-    }
-  };
-
-  if (hasPermission === null) {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.instruction}>Demande de permission pour la caméra...</Text>
-      </View>
-    );
-  }
-
-  if (hasPermission === false) {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.instruction}>Pas d'accès à la caméra</Text>
-        <Button title={'Autoriser la caméra'} onPress={() => askForCameraPermission()} />
-      </View>
-    );
-  }
-
-  if (!loggedIn) {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.instruction}>Veuillez scanner le code QR pour vous connecter</Text>
-        <BarCodeScanner
-          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-          style={styles.scanner}
-        />
-        {scanned && (
-          <Button
-            title={'Scanner à nouveau ?'}
-            onPress={() => {
-              setScanned(false);
-              setText('Non encore scanné');
-            }}
-            color="tomato"
-          />
-        )}
-        <Text style={styles.text}>{text}</Text>
-      </View>
-    );
-  }
-
-  if (loggedIn && !selectedBox) {
-    return (
-      <View style={styles.container}>
-        <MapView style={styles.map} initialRegion={{ latitude: 48.8566, longitude: 2.3522, latitudeDelta: 8, longitudeDelta: 8 }}>
-          {data.boxes.map((box) => (
-            <Marker
-              key={box.id}
-              coordinate={{ latitude: box.latitude, longitude: box.longitude }}
-              onPress={() => setSelectedBox(box)}
-            />
-          ))}
-        </MapView>
-        <Text style={styles.instruction}>Bienvenue, {user.firstName} {user.lastName} !</Text>
-        <Text style={styles.instruction}>Sélectionnez une boîte pour voir ses livres</Text>
-        <Text style={styles.text}>{text}</Text>
-      </View>
-    );
-  }
-
-  if (loggedIn && selectedBox && !selectedBook) {
-    const renderBooks = () => {
-      return (
-        <View>
-          <Text style={styles.instruction}>Liste des livres :</Text>
-          {selectedBox.books.map((book) => (
-            <Text key={book.id}>{book.title}</Text>
-          ))}
-        </View>
-      );
+    const askForCameraPermission = async () => {
+        const { status } = await BarCodeScanner.requestPermissionsAsync();
+        setHasPermission(status === 'granted');
     };
 
-    return (
-      <View style={styles.container}>
-        <Text style={styles.instruction}>Boîte : {selectedBox.location}</Text>
-        {renderBooks()}
-        <Text style={styles.instruction}>Sélectionnez un livre en scannant son code QR</Text>
-        <BarCodeScanner
-          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-          style={styles.scanner}
-        />
-        {scanned && (
-          <Button
-            title={'Scanner à nouveau ?'}
-            onPress={() => {
-              setScanned(false);
-              setText('Non encore scanné');
-            }}
-            color="tomato"
-          />
-        )}
-        <Text style={styles.text}>{text}</Text>
-      </View>
-    );
-  }
+    useEffect(() => {
+        askForCameraPermission();
+    }, []);
 
-  if (loggedIn && selectedBox && selectedBook) {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.instruction}>Boîte : {selectedBox.location}</Text>
-        <Text style={styles.instruction}>Titre : {selectedBook.title}</Text>
-        <Text style={styles.instruction}>Auteur : {selectedBook.author}</Text>
-        <Button
-          title={'Scanner un autre livre ?'}
-          onPress={() => {
+    useEffect(() => {
+        if (loggedIn) {
+            setLastLoginTime(Date.now());
+        }
+    }, [loggedIn]);
+
+    const handleBarCodeScanned = ({ data: scannedData }) => {
+        if (!loggedIn && scannedData === '1234567891012345') {
+            setLoggedIn(true);
+            setUser({ firstName: 'Tom', lastName: 'Gérard' });
+            setScanned(false);
+            setText('Connexion réussie !');
+        } else if (loggedIn && selectedBox && !selectedBook) {
+            const bookId = parseInt(scannedData);
+            const selectedBook = selectedBox.books.find((book) => book.id === bookId);
+
+            if (selectedBook) {
+                setSelectedBook(selectedBook);
+                selectedBox.books = selectedBox.books.filter((book) => book.id !== bookId);
+                setScanned(false);
+                setText('Livre sélectionné ! Scannez un autre livre ou déposez le livre actuel.');
+            } else {
+                setScanned(false);
+                setText('ID de livre invalide. Veuillez scanner un autre livre.');
+            }
+        }
+    };
+
+    const handleBookTake = () => {
+        if (selectedBox && selectedBook) {
             setSelectedBook(null);
-            setText('Non encore scanné');
-          }}
-          color="tomato"
-        />
-        <Button title={'Prendre le livre'} onPress={handleBookTake} color="green" />
-        <Button title={'Déposer le livre'} onPress={handleBookDeposit} color="blue" />
-        <Text style={styles.text}>{text}</Text>
-      </View>
-    );
-  }
+            setText('Livre pris avec succès !');
+            // Vérifier le délai de connexion
+            if (lastLoginTime && Date.now() - lastLoginTime > LOGIN_TIMEOUT) {
+                // Si le délai de connexion est dépassé, réinitialiser l'état de connexion
+                setLoggedIn(false);
+                setUser(null);
+                setLastLoginTime(null);
+            } else {
+                // Réinitialiser l'état de la boîte sélectionnée pour revenir à la sélection sur la carte
+                setSelectedBox(null);
+            }
+        }
+    };
 
-  return null;
+    const handleBookDeposit = () => {
+        if (selectedBox && selectedBook) {
+            selectedBox.books.push(selectedBook);
+            setSelectedBook(null);
+            setText('Livre déposé avec succès !');
+        }
+    };
+
+    const renderBooks = () => {
+        return (
+            <View>
+                <Text style={styles.instruction}>Liste des livres :</Text>
+                {selectedBox.books.map((book) => (
+                    <View key={book.id} style={styles.bookContainer}>
+                        <Text style={styles.bookId}>ID: {book.id}</Text>
+                        <Text style={styles.bookTitle}>{book.title}</Text>
+                    </View>
+                ))}
+            </View>
+        );
+    };
+
+    if (hasPermission === null) {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.instruction}>Demande de permission pour la caméra...</Text>
+            </View>
+        );
+    }
+
+    if (hasPermission === false) {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.instruction}>Pas d'accès à la caméra</Text>
+                <Button title={'Autoriser la caméra'} onPress={() => askForCameraPermission()} />
+            </View>
+        );
+    }
+
+    if (!loggedIn) {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.instruction}>Veuillez scanner le code QR pour vous connecter</Text>
+                <BarCodeScanner
+                    onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+                    style={styles.scanner}
+                />
+                {scanned && (
+                    <Button
+                        title={'Scanner à nouveau ?'}
+                        onPress={() => {
+                            setScanned(false);
+                            setText('Non encore scanné');
+                        }}
+                        color="tomato"
+                    />
+                )}
+                <Text style={styles.text}>{text}</Text>
+            </View>
+        );
+    }
+
+    if (loggedIn && !selectedBox) {
+        return (
+            <View style={styles.container}>
+                <MapView style={styles.map} initialRegion={{ latitude: 48.8566, longitude: 2.3522, latitudeDelta: 8, longitudeDelta: 8 }}>
+                    {data.boxes.map((box) => (
+                        <Marker
+                            key={box.id}
+                            coordinate={{ latitude: box.latitude, longitude: box.longitude }}
+                            onPress={() => setSelectedBox(box)}
+                        />
+                    ))}
+                </MapView>
+                <Text style={styles.instruction}>Bienvenue, {user.firstName} {user.lastName} !</Text>
+                <Text style={styles.instruction}>Sélectionnez une boîte pour voir ses livres</Text>
+                <Text style={styles.text}>{text}</Text>
+            </View>
+        );
+    }
+
+    if (loggedIn && selectedBox && !selectedBook) {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.instruction}>Boîte : {selectedBox.location}</Text>
+                {renderBooks()}
+                <Text style={styles.instruction}>Sélectionnez un livre en scannant son code QR</Text>
+                <BarCodeScanner
+                    onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+                    style={styles.scanner}
+                />
+                {scanned && (
+                    <Button
+                        title={'Scanner à nouveau ?'}
+                        onPress={() => {
+                            setScanned(false);
+                            setText('Non encore scanné');
+                        }}
+                        color="tomato"
+                    />
+                )}
+                <Text style={styles.text}>{text}</Text>
+            </View>
+        );
+    }
+
+    if (loggedIn && selectedBox && selectedBook) {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.instruction}>Boîte : {selectedBox.location}</Text>
+                <Text style={styles.instruction}>Titre : {selectedBook.title}</Text>
+                <Text style={styles.instruction}>ID : {selectedBook.id}</Text>
+                <Button
+                    title={'Scanner un autre livre ?'}
+                    onPress={() => {
+                        setSelectedBook(null);
+                        setText('Non encore scanné');
+                    }}
+                    color="tomato"
+                />
+                <Button title={'Prendre le livre'} onPress={handleBookTake} color="green" />
+                <Button title={'Déposer le livre'} onPress={handleBookDeposit} color="blue" />
+                <Text style={styles.text}>{text}</Text>
+            </View>
+        );
+    }
+
+    return null;
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  instruction: {
-    fontSize: 20,
-    marginBottom: 10,
-  },
-  scanner: {
-    width: Dimensions.get('window').width - 20,
-    height: Dimensions.get('window').height / 2,
-    marginBottom: 10,
-  },
-  map: {
-    flex: 1,
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  },
-  text: {
-    fontSize: 18,
-    marginTop: 10,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    instruction: {
+        fontSize: 20,
+        marginBottom: 10,
+    },
+    scanner: {
+        width: Dimensions.get('window').width - 20,
+        height: Dimensions.get('window').height / 2,
+        marginBottom: 10,
+    },
+    map: {
+        flex: 1,
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+    },
+    text: {
+        fontSize: 18,
+        marginTop: 10,
+    },
+
+    bookContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 10, 
+    },
+    bookId: {
+        fontSize: 16,
+        color: 'gray',
+        marginRight: 10,
+        width: 40, 
+    },
+    bookTitle: {
+        fontSize: 16,
+        marginLeft: 10, 
+    },
 });
--- data.js
+++ data.js
@@ -1,121 +1,136 @@
 const data = {
-  boxes: [
-    {
-      id: 1,
-      location: 'Paris',
-      latitude: 48.8566,
-      longitude: 2.3522,
-      books: [
+    boxes: [
         {
-          id: 1,
-          title: 'Book 1',
-          author: 'Author 1',
+            id: 1,
+            location: 'Paris',
+            latitude: 48.8566,
+            longitude: 2.3522,
+            books: [
+                {
+                    id: 1,
+                    title: 'Investir en Bourse',
+                    author: 'John C. Bogle',
+                    synopsis: 'John C. Bogle, fondateur de Vanguard Group, offre des conseils sur l\'investissement en actions et les fonds indiciels pour une croissance financière à long terme.'
+                },
+                {
+                    id: 2,
+                    title: 'L\'investisseur intelligent',
+                    author: 'Benjamin Graham',
+                    synopsis: 'Benjamin Graham, le mentor de Warren Buffett, explique les principes de l\'investissement valeur et comment choisir des actions sous-évaluées.'
+                },
+                {
+                    id: 3,
+                    title: 'Investir dans l\'immobilier',
+                    author: 'Robert Kiyosaki',
+                    synopsis: 'Robert Kiyosaki partage des conseils sur la création de richesse à travers l\'investissement immobilier, y compris les biens locatifs et les investissements à revenu passif.'
+                }
+            ]
         },
         {
-          id: 2,
-          title: 'Book 2',
-          author: 'Author 2',
+            id: 2,
+            location: 'Lyon',
+            latitude: 45.75,
+            longitude: 4.85,
+            books: [
+                {
+                    id: 4,
+                    title: 'La psychologie de l\'argent',
+                    author: 'Morgan Housel',
+                    synopsis: 'Morgan Housel explore la psychologie derrière les décisions financières et comment comprendre notre propre comportement peut influencer notre succès financier.'
+                },
+                {
+                    id: 5,
+                    title: 'Investir pour les nuls',
+                    author: 'Eric Tyson',
+                    synopsis: 'Eric Tyson offre un guide accessible pour les débutants en investissement, couvrant les concepts de base et les stratégies pour construire une base financière solide.'
+                },
+                {
+                    id: 6,
+                    title: 'Investir dans les startups',
+                    author: 'Peter Thiel',
+                    synopsis: 'Peter Thiel partage des idées sur l\'investissement dans les startups innovantes et comment choisir des opportunités qui peuvent créer un impact significatif.'
+                }
+            ]
         },
         {
-          id: 3,
-          title: 'Book 3',
-          author: 'Author 3',
-        },
-      ],
-    },
-    {
-      id: 2,
-      location: 'Lyon',
-      latitude: 45.75,
-      longitude: 4.85,
-      books: [
-        {
-          id: 4,
-          title: 'Book 4',
-          author: 'Author 4',
-        },
-        {
-          id: 5,
-          title: 'Book 5',
-          author: 'Author 5',
-        },
-        {
-          id: 6,
-          title: 'Book 6',
-          author: 'Author 6',
-        },
-      ],
-    },
-    {
-      id: 3,
-      location: 'Bordeaux',
-      latitude: 44.8378,
-      longitude: -0.5792,
-      books: [
-        {
-          id: 7,
-          title: 'Book 7',
-          author: 'Author 7',
+            id: 3,
+            location: 'Bordeaux',
+            latitude: 44.8378,
+            longitude: -0.5792,
+            books: [
+                {
+                    id: 7,
+                    title: 'Investir dans les matières premières',
+                    author: 'Jim Rogers',
+                    synopsis: 'Jim Rogers, un investisseur légendaire, explore les marchés des matières premières et comment investir dans des ressources naturelles comme l\'or et le pétrole.'
+                },
+                {
+                    id: 8,
+                    title: 'Investir dans l\'art',
+                    author: 'Evan Beard',
+                    synopsis: 'Evan Beard offre des conseils sur l\'investissement dans l\'art, y compris comment évaluer, acheter et conserver des œuvres d\'art pour un potentiel de croissance.'
+                },
+                {
+                    id: 9,
+                    title: 'Investir dans la technologie',
+                    author: 'Peter Lynch',
+                    synopsis: 'Peter Lynch explique sa stratégie d\'investissement dans les entreprises technologiques et comment repérer les tendances émergentes dans le secteur.'
+                }
+            ]
         },
         {
-          id: 8,
-          title: 'Book 8',
-          author: 'Author 8',
+            id: 4,
+            location: 'Toulouse',
+            latitude: 43.6047,
+            longitude: 1.4442,
+            books: [
+                {
+                    id: 10,
+                    title: 'Investir dans l\'immobilier commercial',
+                    author: 'David M. Geltner',
+                    synopsis: 'David M. Geltner offre un aperçu de l\'investissement dans l\'immobilier commercial, y compris les bureaux, les centres commerciaux et les propriétés industrielles.'
+                },
+                {
+                    id: 11,
+                    title: 'Investir dans les dividendes',
+                    author: 'Matthew R. Kratter',
+                    synopsis: 'Matthew R. Kratter explique la stratégie d\'investissement axée sur les dividendes et comment choisir des actions offrant un rendement constant.'
+                },
+                {
+                    id: 12,
+                    title: 'Investir dans les biotechs',
+                    author: 'Albert P. Li',
+                    synopsis: 'Albert P. Li se penche sur l\'investissement dans les entreprises biotechnologiques et les opportunités dans le secteur de la santé.'
+                }
+            ]
         },
         {
-          id: 9,
-          title: 'Book 9',
-          author: 'Author 9',
-        },
-      ],
-    },
-    {
-      id: 4,
-      location: 'Toulouse',
-      latitude: 43.6047,
-      longitude: 1.4442,
-      books: [
-        {
-          id: 10,
-          title: 'Book 10',
-          author: 'Author 10',
-        },
-        {
-          id: 11,
-          title: 'Book 11',
-          author: 'Author 11',
-        },
-        {
-          id: 12,
-          title: 'Book 12',
-          author: 'Author 12',
-        },
-      ],
-    },
-    {
-      id: 5,
-      location: 'Lille',
-      latitude: 50.6292,
-      longitude: 3.0573,
-      books: [
-        {
-          id: 13,
-          title: 'Book 13',
-          author: 'Author 13',
-        },
-        {
-          id: 14,
-          title: 'Book 14',
-          author: 'Author 14',
-        },
-        {
-          id: 15,
-          title: 'Book 15',
-          author: 'Author 15',
-        },
-      ],
-    },
-  ],
+            id: 5,
+            location: 'Lille',
+            latitude: 50.6292,
+            longitude: 3.0573,
+            books: [
+                {
+                    id: 13,
+                    title: 'Investir dans les fonds communs de placement',
+                    author: 'John C. Bogle',
+                    synopsis: 'John C. Bogle explique les avantages des fonds indiciels et comment les investisseurs peuvent diversifier leur portefeuille à moindre coût.'
+                },
+                {
+                    id: 14,
+                    title: 'Investir dans l\'énergie renouvelable',
+                    author: 'David J. Cichon',
+                    synopsis: 'David J. Cichon explore l\'investissement dans les énergies renouvelables, y compris l\'énergie solaire, éolienne et hydroélectrique.'
+                },
+                {
+                    id: 15,
+                    title: 'Investir dans les marchés émergents',
+                    author: 'Mark Mobius',
+                    synopsis: 'Mark Mobius offre des conseils sur l\'investissement dans les marchés émergents, y compris les opportunités et les défis de ces économies en croissance.'
+                }
+            ]
+        }
+    ]
 };
 
 export default data;
